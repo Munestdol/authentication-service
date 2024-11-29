0986539d1f23037caf053c56f78738277f687876
Pavel Kirillov <75166942+Munestdol@users.noreply.github.com>
2024-11-29 11:53:58 +0300

    Update main.go

--- cmd/authentication-service/main.go
+++ cmd/authentication-service/main.go
@@ -31,7 +31,7 @@ func main() {
 	}
 
 	repo := repository.NewRepository(db)
-	services := service.NewService(repo, cfg)
+	services := service.NewService(repo)
 	handlers := handler.NewHandler(services)
 	srv := server.NewServer(cfg, handlers.InitRoutes())
 
