9a7e937f9398109f49a4e01da753b5efbf497449
Pavel Kirillov <75166942+Munestdol@users.noreply.github.com>
2024-11-29 11:53:53 +0300

    Update auth.go

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,1 +1,62 @@
 package handler
+
+import (
+	"authentication-service/internal/domain"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) Login(c *gin.Context) {
+	var creds domain.Credentials
+	if err := c.BindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	tokenString, err := h.services.Auth.Login(creds)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not create token"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
+
+func (h *Handler) Authenticate(c *gin.Context) {
+	tokenString := c.GetHeader("Authorization")
+	if tokenString == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+		c.Abort()
+		return
+	}
+
+	jwtKey := h.services.Auth.Auth()
+
+	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+	claims := &domain.Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	log.Print(err)
+	log.Print(token)
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		c.Abort()
+		return
+	}
+
+	c.Set("username", claims.Username)
+	c.Next()
+
+}
+
+func (h *Handler) ProtectedEndpoint(c *gin.Context) {
+	username := c.MustGet("username").(string)
+	c.JSON(http.StatusOK, gin.H{"message": "Valid token, welcome " + username})
+}
